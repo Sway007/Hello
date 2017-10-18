bdbe47ed3a2ec96f20c02fadd071bc5189103284
Sway007 <yansw007@gmail.com>
2017-10-18 11:38:40 +0800

    update

--- ML_Lab2/k-means.py
+++ ML_Lab2/k-means.py
@@ -1,8 +1,22 @@
 import numpy as np 
 import scipy.spatial.distance as distance
 import random
+import csv
 
 import matplotlib.pyplot as plt
+
+# iterN = 20
+
+def readCSVDatas(fileName):
+
+    with open(fileName) as csvfile:
+
+        reader = csv.reader(csvfile)
+
+        infos = [row[0].split() for row in reader]
+        retDatas = [[float(i) for i in info[1:]] for info in infos[1:-1]]
+
+    return np.array(retDatas)
 
 
 def getInitCenters(datas, k):
@@ -49,15 +63,55 @@ def k_means_2d(datas, centerNum):
     '''
     centers = getInitCenters(datas, centerNum)
 
-    return clustering(centers, datas, 10)
+    return clustering(centers, datas, 20)
 
 
-def drawPoints(points, style, axis):
+def clustering_medoids(centers, datas, iterNum):
+
+    '''
+    centers: numpy.array
+    datas: numpy.array
+    return clusters:tuple(centers[], [[c1points], [c2points], ...])
+    '''
+    while iterNum > 0:
+
+        retCluster = [[] for i in centers]
+
+        disMatrix = distance.cdist(datas, centers, metric='euclidean')
+        maxInfo = np.argmin(disMatrix, axis=1)
+
+        for i in range(datas.shape[0]):
+
+            cIndex = maxInfo[i]
+            retCluster[cIndex].append( datas[i] )
+
+        for cluster in retCluster:
+
+            disMatrix_center = distance.cdist(cluster, cluster)
+            sum = np.sum(disMatrix_center, axis=0)
+            c = np.max(sum)
+
+
+        iterNum -= 1
+
+    return centers, retCluster
+
+def k_medoids_2d(datas, centerNum):
+    '''
+    :param datas: numpy.array
+    :return: same as k_means_2d
+    '''
+    centers = getInitCenters(datas, centerNum)
+
+
+
+
+def drawPoints(points, style, axis, markersize=3.):
 
     points = np.array(points)
     x = points[:, 0]
     y = points[:, 1]
-    axis.plot(x, y, style)
+    axis.plot(x, y, style, markersize=markersize)
 
 def draw(datas, centers):
 
@@ -72,25 +126,17 @@ def draw(datas, centers):
         drawPoints(oneCluster, colors[colori] + 'o', ax)
         colori += 1
 
-    # tmp = np.concatenate(datas[:], axis=0)
-    # drawPoints(tmp, 'bo', ax)
-
-    drawPoints(centers, 'k*', ax)
-
-    plt.show()
+    drawPoints(centers, 'k*', ax, markersize=5.)
 
 
 if __name__ == '__main__':
 
     size = 1000
-    k = 6
+    k = 4
 
-    xarray = np.random.randint(50000, size=size).reshape(size, 1)
-    yarray = np.random.randint(50000, size=size).reshape(size, 1)
-
-    ndatas = np.concatenate((xarray, yarray), axis=1)
-
+    ndatas = readCSVDatas('data_1024.csv')
 
     centers, clusters = k_means_2d(ndatas, k)
-
     draw(clusters, centers)
+
+    plt.show()
