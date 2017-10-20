82b2998db7e71b1afadac0b9be995eeed5c3b39f
Sway007 <yansw007@gmail.com>
2017-10-20 19:42:58 +0800

    update

--- ML_Lab2/GMM.py
+++ ML_Lab2/GMM.py
@@ -10,7 +10,7 @@ def getGaussianDisPro(varX, mu, sigma):
     :return: the probability of varX in distribution
     '''
     _, n = np.mat(varX).shape
-    A = np.pow(2 * np.pi, n/2.) * np.pow( np.linalg.det(np.mat(mu)), 0.5 )
+    A = np.power(2 * np.pi, n / 2.) * np.power( np.linalg.det(np.mat(sigma)), 0.5 )
     A = 1 / A
     B = np.exp( -0.5 * np.mat(varX - mu) * np.mat(sigma) *
                 np.mat(varX - mu).I )
@@ -19,9 +19,9 @@ def getGaussianDisPro(varX, mu, sigma):
 
 class GMM:
 
-    def __init__(self, dimension):
+    def __init__(self, modeNum):
 
-        self.d = dimension
+        self.n = modeNum
 
         # mode arguments
         self._alphas = None
@@ -32,16 +32,16 @@ class GMM:
 
     def modeInit(self):
 
-        self._alphas = np.array([1. / self.d for i in range(self.d)])
-        self._mus = np.random.randint(0, 3*self.d, size=self.d)
-        self._varianceMatrix = np.eye(self.d, k=0, dtype=float) * 0.1
+        self._alphas = np.array([1. / self.n for i in range(self.n)])
+        self._mus = np.random.randint(0, 3 * self.n, size=self.n)
+        self._varianceMatrix = np.eye(self.n, k=0, dtype=float) * 0.1
 
     def trainMode(self, trainingDatas):
 
         '''
         :param trainingDatas: expeted type numpy.array
         '''
-        assert (trainingDatas[0].shape == 1, self.d), 'training data dimension \
+        assert (trainingDatas[0].shape == 1, self.n), 'training data dimension \
                                                        not matched'
         gammaPosteriorArray = np.array([])
 
